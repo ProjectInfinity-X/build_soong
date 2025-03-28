47e0e3e930e3a612ab9d48089f53d62d99ce2a70
Michael Bestas <mkbestas@lineageos.org>
2025-03-28 20:50:20 +0000

    soong: Adapt for Infinity-X

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -43,7 +43,7 @@ def get_build_keys(product_config):
   default_cert = product_config.get("DefaultAppCertificate", "")
   if default_cert == "" or default_cert == os.path.join(TEST_KEY_DIR, "testKey"):
     return "test-keys"
-  if default_cert.startswith("vendor/lineage-priv/"):
+  if default_cert.startswith("vendor/infinity-priv/"):
     return "release-keys"
   return "dev-keys"
 
@@ -127,8 +127,8 @@ def parse_args():
   if args.build_thumbprint_file:
     config["BuildThumbprint"] = args.build_thumbprint_file.read().strip()
 
-  config["LineageDesc"] = config["BuildDesc"]
-  config["LineageDevice"] = config["DeviceName"]
+  config["InfinityDesc"] = config["BuildDesc"]
+  config["InfinityDevice"] = config["DeviceName"]
 
   if config["BuildNumber"].startswith("eng."):
     config["BuildNumber"] = config["DateUtc"]
@@ -221,7 +221,7 @@ def generate_build_info(args):
       print(f"ro.build.display.id?={config['BuildId']} {config['BuildKeys']}")
   else:
     # Non-user builds should show detailed build information (See build desc above)
-    print(f"ro.build.display.id?={config['LineageDesc']}")
+    print(f"ro.build.display.id?={config['InfinityDesc']}")
   print(f"ro.build.version.incremental={config['BuildNumber']}")
   print(f"ro.build.version.sdk={config['Platform_sdk_version']}")
   print(f"ro.build.version.sdk_full={config['Platform_sdk_version_full']}")
@@ -249,7 +249,7 @@ def generate_build_info(args):
   # flavor (via a dedicated lunch config for example).
   print(f"ro.build.flavor={config['BuildFlavor']}")
 
-  print(f"ro.lineage.device={config['LineageDevice']}")
+  print(f"ro.infinity.device={config['InfinityDevice']}")
 
   # These values are deprecated, use "ro.product.cpu.abilist"
   # instead (see below).
