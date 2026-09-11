baikalos/android_build_soong

soong: Overriding lineage namespace
Change-Id: I1849776dbf97a90b49ce3c02cf253f87e0d75bf6
Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

java/app.go
@@ -1574,7 +1574,9 @@ func AutogeneratedRroModuleName(ctx android.EarlyModuleContext, moduleName, part
 	// So, the original system module does not need to know the target product
 	// name. Instead, add a reverse dependency from the rro to the original
 	// system module to see if the module exists.
-	return fmt.Sprintf("%s__%s__auto_generated_rro_%s", moduleName, ctx.Config().Getenv("TARGET_PRODUCT"), partition)
+	product := strings.Replace(ctx.Config().Getenv("TARGET_PRODUCT"), "lineage_", "crdroid_", 1)
+	overrideName := strings.Replace(strings.Replace(strings.Replace(moduleName, "lineageos", "crdroid", -1), "Lineage", "crDroid", -1), "lineage", "crdroid", -1)
+	return fmt.Sprintf("%s__%s__auto_generated_rro_%s", overrideName, product, partition)
 }
 
 func AutogeneratedProductCharacteristicsRroModuleName(ctx android.EarlyModuleContext, moduleName string) string {

ProjectInfinity-X/build_soong

java/app.go
@@ -1676,7 +1676,9 @@ func AutogeneratedRroModuleName(ctx android.EarlyModuleContext, moduleName, part
 	// So, the original system module does not need to know the target product
 	// name. Instead, add a reverse dependency from the rro to the original
 	// system module to see if the module exists.
-	return fmt.Sprintf("%s__%s__auto_generated_rro_%s", moduleName, ctx.Config().Getenv("TARGET_PRODUCT"), partition)
+	product := strings.Replace(ctx.Config().Getenv("TARGET_PRODUCT"), "lineage_", "infinity_", 1)
+	overrideName := strings.Replace(strings.Replace(strings.Replace(moduleName, "lineageos", "infinity", -1), "Lineage", "ProjectInfinityX", -1), "lineage", "infinity", -1)
+	return fmt.Sprintf("%s__%s__auto_generated_rro_%s", overrideName, product, partition)
 }
 
 func AutogeneratedProductCharacteristicsRroModuleName(ctx android.EarlyModuleContext, moduleName string) string {